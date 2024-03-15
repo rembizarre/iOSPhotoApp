6cc7ac297ff4ec62c6db1d58048f3504802c7d1e
Remi <usrustem@gmail.com>
2024-03-15 13:06:26 +0500

    Initial project Setup

---
+++ iOSPhotoApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  iOSPhotoApp
+//
+//  Created by Rustem on 15.03.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
