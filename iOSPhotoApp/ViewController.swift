e32e36186c4c2babcfd3e89296e96233b1cbe251
Remi <usrustem@gmail.com>
2024-03-15 13:01:55 +0500

    Initial Commit

---
+++ iOSPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPhotoApp
+//
+//  Created by Rustem on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
